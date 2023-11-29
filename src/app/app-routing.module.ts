18e287e7a9ece8bcf2cd58e7ac846efa659ab9b7
VitorAndes <vitor.andes.santos04@gmail.com>
2023-11-29 01:44:06 -0400

    First commit - adição About, mobile-first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CardComponent } from './pages/portfolio/card/card.component';
+import { AboutComponent } from './pages/index/about/about.component';
+
+const routes: Routes = [
+  {path:'', component:AboutComponent, pathMatch:'full'},
+  {path:'portfolio', component:CardComponent},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
